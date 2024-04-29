58e1f28372bb5b3ef6a483f210bb10447819ab79
Anton <example@mail.ru>
2024-04-29 13:16:19 +0300

    1.23. Начинаем программировать

---
+++ src/View/new-event-button.js
@@ -0,0 +1,4 @@
+import {createElement} from '../render.js';
+
+const NewEventButtonView = () => '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
+
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+
+const EventTypeItem = {
+  TAXI: 'taxi',
+  BUS: 'bus',
+  TRAIN: 'train',
+  SHIP: 'ship',
+  DRIVE:'drive',
+  FLIGHT: 'flight',
+  CHECKIN: 'check-in',
+  SIGHTSEEING: 'sightseeing',
+  RESTAURANT: 'restaurant',
+};
