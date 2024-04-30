51a9fac00e4db398cdb32d944ed0b6e3f7bcd8a7
Anton <example@mail.ru>
2024-04-30 19:00:19 +0300

    1.24. Время разбивать камни

--- src/View/new-event-button.js
+++
@@ -1,4 +0,0 @@
-import {createElement} from '../render.js';
-
-const NewEventButtonView = () => '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
-
---
+++ src/view/sort-view.js
@@ -0,0 +1,31 @@
+import { createElement } from '../render.js';
+import { SORT_TYPES } from '../constants.js';
+
+const createSortItemTemplate = (type) => `
+<div class="trip-sort__item  trip-sort__item--${type}">
+  <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" checked>
+  <label class="trip-sort__btn" for="sort-${type}">${type}</label>
+</div>`;
+
+const createFilterViewTemplate = () => `
+<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+${SORT_TYPES.map((type) => createSortItemTemplate(type)).join('')}
+</form>`;
+
+export default class SortView {
+  getTemplate() {
+    return createFilterViewTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
