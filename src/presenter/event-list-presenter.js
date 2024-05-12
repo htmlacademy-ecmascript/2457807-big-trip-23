7d377bf0d4b4362e00131454e5af3ba71dd799ad
Anton <example@mail.ru>
2024-05-12 20:18:16 +0300

    4.4  Выделит показ точек в функцию

--- src/presenter/event-list-presenter.js
+++ src/presenter/event-list-presenter.js
@@ -9,30 +9,38 @@ export default class EventListPresenter {
   #eventsModel = null;
   #eventListComponent = new EventListView();
 
+  #boardEvents = [];
+  #boardDestinations = [];
+  #boardOffers = [];
+
   constructor({eventListContainer, eventsModel}) {
     this.#eventListContainer = eventListContainer;
     this.#eventsModel = eventsModel;
   }
 
+  #renderEvent(eventData, destinationData, offersData) {
+    const eventComponent = new EventView({eventData, destinationData, offersData});
+    render(eventComponent, this.#eventListComponent.element);
+  }
+
   init() {
-    this.boardEvents = [...this.#eventsModel.events];
-    this.boardDestinations = [...this.#eventsModel.destinations];
-    this.boardOffers = [...this.#eventsModel.offers];
+    this.#boardEvents = [...this.#eventsModel.events];
+    this.#boardDestinations = [...this.#eventsModel.destinations];
+    this.#boardOffers = [...this.#eventsModel.offers];
+
+
     render(this.#eventListComponent, this.#eventListContainer);
-    render(new FormEventView({eventData: this.boardEvents[FORM_EVENT_INDEX],
-      destinationsData: this.boardDestinations,
-      offersData: this.boardOffers}), this.#eventListComponent.element);
-    for (let i = 1; i < this.boardEvents.length; i++) {
-      // const indexDestination = this.boardDestinations.findIndex((destination) => destination.id === this.boardEvents[i].destination);
-      // const indexOffers = this.boardOffers.findIndex((offer) => offer.type === this.boardEvents[i].type);
-      render(new EventView({
-        eventData: this.boardEvents[i],
-        destinationData: this.#eventsModel.getDestinationById(this.boardEvents[i].destination),
-        // destinationsData: this.boardDestinations[indexDestination],
-        offersData: this.#eventsModel.getOffersByType(this.boardEvents[i].type),
-        // offersData: this.boardOffers[indexOffers]
-      }),
-      this.#eventListComponent.element);
+
+    render(new FormEventView({eventData: this.#boardEvents[FORM_EVENT_INDEX],
+      destinationsData: this.#boardDestinations,
+      offersData: this.#boardOffers}),
+    this.#eventListComponent.element);
+
+    for (let i = 1; i < this.#boardEvents.length; i++) {
+      this.#renderEvent(
+        this.#boardEvents[i],
+        this.#eventsModel.getDestinationById(this.#boardEvents[i].destination),
+        this.#eventsModel.getOffersByType(this.#boardEvents[i].type));
     }
   }
 }
