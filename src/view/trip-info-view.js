576777bca063bb4d5c88e3ef7b11d8b59bdf3b32
Anton <example@mail.ru>
2024-05-19 03:48:54 +0300

    5.10. заготовка trip info date

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -2,14 +2,17 @@ import AbstractView from '../framework/view/abstract-view.js';
 import { formatTime, formatDateForm, formatDateTripInfo } from '../utils/utils.js';
 
 const CreateTripInfoViewTemplate = (total, tripTitle, tripInfoTime) =>{
+  const [fistCity, secondCity,] = tripTitle;
+  const endCity = tripTitle[tripTitle.length - 1];
   const [dateStart, dateEnd] = tripInfoTime;
+  
   return `<div class="trip-main">
 <section class="trip-main__trip-info  trip-info">
   <div class="trip-info__main">
     <h1 class="trip-info__title">
-    ${tripTitle.length > 0 ? tripTitle[0] : ''} 
-    ${tripTitle.length !== 3 ? ( tripTitle.length > 3 ? ' &mdash;' + ' . . . ' + ' &mdash; '   : '&mdash;') : ' &mdash; ' + tripTitle[1] + ' &mdash; '} 
-    ${tripTitle.length > 0 ?  tripTitle[tripTitle.length-1] : ''}</h1>
+    ${tripTitle.length > 0 ? fistCity : ''} 
+    ${tripTitle.length !== 3 ? ( tripTitle.length > 3 ? ' &mdash;' + ' . . . ' + ' &mdash; '   : '&mdash;') : ' &mdash; ' + secondCity + ' &mdash; '} 
+    ${tripTitle.length > 0 ?  endCity : ''}</h1>
 
     <p class="trip-info__dates">${formatDateTripInfo(dateStart)}&nbsp;&mdash;&nbsp;${formatDateTripInfo(dateEnd)}</p>
   </div>
