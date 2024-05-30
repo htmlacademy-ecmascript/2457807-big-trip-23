3b13d41441598c66394f55edd7333accc10ca7a6
Anton <example@mail.ru>
2024-05-30 20:27:25 +0300

    7.10. Меняй-удаляй

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,4 +1,4 @@
-const getRandomArrayElement = (items) =>items[Math.floor(Math.random() * items.length)];
+const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
