f4bcc180422f943825475057768a43129e21bfbc
Anton <example@mail.ru>
2024-06-04 22:25:42 +0300

    Прохождение тестов.

--- src/view/form-event-view.js
+++ src/view/form-event-view.js
@@ -7,8 +7,8 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const BLANK_EVENT = {
   basePrice: 0,
-  dateFrom:new Date(),
-  dateTo: new Date(new Date().valueOf() + 1000 * 60),
+  dateFrom:'',
+  dateTo: '',
   destination: '',
   isFavorite: false,
   type: 'flight',
@@ -114,10 +114,10 @@ const createFormEventTemplate = (destinationsData, offersData, state) =>{
 
     <div class="event__field-group  event__field-group--time">
       <label class="visually-hidden" for="event-start-time-1">From</label>
-      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${formatDateForm(dateFrom)}">
+      <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" requed value="${ id === 0 ? '' : formatDateForm(dateFrom)}">
       &mdash;
       <label class="visually-hidden" for="event-end-time-1">To</label>
-      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${formatDateForm(dateTo)}">
+      <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" requed value="${id === 0 ? '' : formatDateForm(dateTo)}">
     </div>
 
     <div class="event__field-group  event__field-group--price">
